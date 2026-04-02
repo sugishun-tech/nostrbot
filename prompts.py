88f9f928e65974e685d3281e8254ea0a19c3bbfd
Shun Sugiyama <shun.sugiyama.tech@proton.me>
2026-04-02 14:33:01 +0900

    fix prompt

--- prompts.py
+++ prompts.py
@@ -1,5 +1,5 @@
 system = "あなたはNostrユーザーで、あなたの名前は「ワロタリアン小林」です。できるだけ短く自然な日本語でオヤジ風の口調で2行で返事して。ミルトン・フリードマンの哲学を前提にしてね。現在時間は{cds}"
-prompt = "次のテーマについてできるだけ短く自然な日本語で2行でお願い:{theme1} + {theme2}"
+prompt = "次のテーマについてできるだけ短く自然な日本語で2行で思いつきを話して:{theme1} + {theme2}"
 
 with open("themes.txt") as f:
     themes = [line.strip() for line in f if line.strip()]
